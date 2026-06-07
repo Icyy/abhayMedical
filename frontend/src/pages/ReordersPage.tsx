7a7be1f470b1586e5c79c9da8d3045140c994a26
Icyy <adityaauty9@gmail.com>
2026-06-07 20:37:10 +0530

    feat: add reorders and customers pages

--- frontend/src/pages/ReordersPage.tsx
+++ frontend/src/pages/ReordersPage.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { useInventoryStore } from "../store/inventoryStore";
+import type { Medicine } from "../types/inventory";
 
 const ReordersPage = () => {
+  const medicines = useInventoryStore((state) => state.medicines);
+  const reorders = medicines.filter((med) => med.status === "critical"|| med.status==='low');
+  const getStatusClass = (status: Medicine["status"]) => {
+    if (status === "critical") return "bg-red-100 text-red-800";
+    if (status === "low") return "bg-yellow-100 text-yellow-800";
+    return "bg-green-100 text-green-800";
+  };
   return (
-    <div>ReordersPage</div>
-  )
-}
+    <div className="bg-white rounded-lg border border-gray-200 p-4">
+      <table className="w-full">
+        <thead>
+          <tr className="border-b border-gray-200">
+            <th className="text-left p-3 text-sm text-gray-500">Name</th>
+            <th className="text-left p-3 text-sm text-gray-500">Stock</th>
+            <th className="text-left p-3 text-sm text-gray-500">Unit</th>
+            <th className="text-left p-3 text-sm text-gray-500">Status</th>
+            <th className="text-left p-3 text-sm text-gray-500">Reordered</th>
+          </tr>
+        </thead>
+        <tbody>
+          {reorders.map((med) => (
+            <tr className="border-b border-gray-200" key={med.batchNumber}>
+              <td className="p-3 text-sm">{med.name}</td>
+              <td className="p-3 text-sm">{med.stock}</td>
+              <td className="p-3 text-sm">{med.unit}</td>
+              <td>
+                <span
+                  className={`${getStatusClass(med.status)} p-3 px-2 py-1 rounded-full font-normal text-xs`}
+                >
+                  {med.status}
+                </span>
+              </td>
+              <td className="p-3 text-sm">
+                <button className="bg-green-50 text-green-700 border border-green-200 px-3 py-1 rounded-md text-xs font-medium hover:bg-green-100">
+                  Mark as Reordered
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default ReordersPage
+export default ReordersPage;
