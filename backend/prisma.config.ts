ecf6b1d9812cbfb9212c304404f469956edf75bf
Icyy <adityaauty9@gmail.com>
2026-06-28 15:55:07 +0530

    fix: seed script

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'prisma/config'
-import { PrismaPg } from '@prisma/adapter-pg'
-import dotenv from 'dotenv'
+import { defineConfig } from "prisma/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 export default defineConfig({
+  migrations: {
+    seed: "bun·./prisma/seed.ts",
+  },
   datasource: {
     adapter: new PrismaPg({
-      connectionString: process.env.DATABASE_URL!
+      connectionString: process.env.DATABASE_URL!,
     }),
     url: process.env.DATABASE_URL!,
   },
-})
+});
